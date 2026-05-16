11a28d58394ea597e68de35a41e7132b167f4dd7
Spoorthi1003 <130477860+Spoorthi1003@users.noreply.github.com>
2026-05-16 14:37:16 +0530

    Add files via upload

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -22,15 +22,13 @@ HAVING average_rating IN (1,5)
 AND review_count > 5
 ORDER BY review_count DESC;
 
--- Creating fraud_users view
-CREATE VIEW fraud_users_view AS 
+-- Creating scoring_users view
+CREATE VIEW scoring_users AS 
 WITH user_stats AS(
 SELECT user_id , AVG(rating) AS average_rating,
 COUNT(*) AS review_count
 FROM reviews
 GROUP BY user_id 
-HAVING average_rating IN (1,5) AND 
-review_count > 5
 ), 
 daily_spam AS (
 SELECT user_id, MAX(review_per_day) AS max_review_per_day
@@ -38,53 +36,79 @@ FROM (
 SELECT user_id, review_date , 
 COUNT(*) AS review_per_day
 FROM reviews
-GROUP BY user_id, review_date
-HAVING review_per_day > 7 ) t
+GROUP BY user_id, review_date ) t
 GROUP BY user_id
 ),
-fraud_users AS (
+score AS (
 SELECT u.user_id, u.average_rating,
-d.max_review_per_day ,
-(CASE WHEN u.average_rating IN (1,5) THEN 1 ELSE 0 END +
+COALESCE(d.max_review_per_day,0) AS max_review_per_day ,
+(CASE WHEN u.average_rating <= 1.5 OR u.average_rating >= 4.5
+ THEN 1 ELSE 0 END +
 CASE WHEN u.review_count > 20 THEN 1 ELSE 0 END +
-CASE WHEN d.max_review_per_day > 7 THEN 1 ELSE 0 END)
+CASE WHEN COALESCE(d.max_review_per_day,0) > 7 THEN 1 ELSE 0 END)
 AS fraud_score
-FROM user_stats u JOIN
+FROM user_stats u LEFT JOIN
 daily_spam d ON
 u.user_id = d.user_id 
 ORDER BY fraud_score DESC)
-SELECT * FROM fraud_users;
+SELECT * FROM score;
 
 -- Check view
-SELECT * FROM fraud_users_view;
+SELECT * FROM scoring_users;
 
- -- Most affected products
-SELECT r.product_id , COUNT(*) AS suspicious_reviews
-FROM fraud_users_view f
-JOIN reviews r
-ON r.user_id = f.user_id
-WHERE f.fraud_score >=2 
+ -- Top 5 products with most suspicious reviews
+WITH suspicious_reviews AS(
+SELECT r.product_id, COUNT(*) AS suspicious_review_cnt
+FROM reviews r JOIN
+scoring_users s 
+ON s.user_id = r.user_id
+WHERE s.fraud_score >= 2
 GROUP BY r.product_id
-ORDER BY suspicious_reviews DESC
-LIMIT 10;
+),
+rankings AS (
+SELECT * , DENSE_RANK() OVER 
+(ORDER BY suspicious_review_cnt DESC) AS ranks
+FROM suspicious_reviews)
+SELECT product_id, suspicious_review_cnt
+FROM rankings
+WHERE ranks <=5
+ORDER BY suspicious_review_cnt DESC ;
 
--- Percentage of suspicious reviews for most suspicious products
+-- Top 5 products with highest % suspicious reviews
 WITH total_reviews AS (
 SELECT product_id, COUNT(*) AS total
 FROM reviews
 GROUP BY product_id
-)
-SELECT r.product_id, COUNT(*) AS suspicious_reviews,
-t.total, CONCAT(ROUND(COUNT(*)*100/t.total,2),'%')
+),
+suspicious_reviews AS (
+SELECT r.product_id, t.total, 
+COUNT(*) AS suspicious_review_cnt,
+CONCAT(ROUND(COUNT(*)*100.0/t.total,1),'%') 
 AS percent_suspicious_reviews
-FROM reviews r
-JOIN total_reviews t
-ON t.product_id = r.product_id
-JOIN fraud_users_view f
-ON f.user_id = r.user_id
-WHERE f.fraud_score >=2 AND t.total >= 20
-GROUP BY r.product_id 
-ORDER BY ROUND(COUNT(*)*100/t.total,2) DESC;
+FROM reviews r JOIN total_reviews t
+ON r.product_id = t.product_id 
+JOIN scoring_users s 
+ON r.user_id = s.user_id
+WHERE s.fraud_score >=2 AND t.total > 20
+GROUP BY r.product_id, t.total
+), 
+ranking AS (
+SELECT * , DENSE_RANK() OVER 
+(ORDER BY percent_suspicious_reviews DESC) AS ranks
+FROM suspicious_reviews
+)
+SELECT product_id, total , suspicious_review_cnt,
+percent_suspicious_reviews
+FROM ranking
+WHERE ranks <= 5
+ORDER BY percent_suspicious_reviews DESC;
+
+-- Total suspicious reviews
+SELECT COUNT(*) AS suspicious_reviews
+FROM reviews r 
+JOIN scoring_users s 
+ON s.user_id = r.user_id
+WHERE s.fraud_score>=2;
 
 -- Text duplication
 SELECT review_text , COUNT(*) AS occurance
@@ -102,14 +126,13 @@ HAVING COUNT(*) > 20
 ORDER BY review_per_day DESC;
 
 -- Distribution of ratings
-SELECT f.fraud_score , r.rating,
+SELECT s.fraud_score , r.rating,
 COUNT(*) AS review_count
-FROM fraud_users_view f JOIN
+FROM scoring_users s JOIN
 reviews r
-ON r.user_id = f.user_id
-WHERE f.fraud_score >= 2
-GROUP BY f.fraud_score, r.rating
-ORDER BY COUNT(*) DESC;
+ON r.user_id = s.user_id
+GROUP BY s.fraud_score, r.rating
+ORDER BY s.fraud_score DESC;
 
 -- Time gap between first and last review
 SELECT user_id , MIN(review_date) AS first_active,
