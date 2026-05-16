5a954ec2e462609833eb44ff4ff64856102e82da
Spoorthi1003 <130477860+Spoorthi1003@users.noreply.github.com>
2026-05-16 14:32:02 +0530

    Add files via upload

--- python/charts.py
+++ python/charts.py
@@ -10,12 +10,12 @@ conn = mysql.connector.connect (
     database = "fake_review_detector"
 )
 
-# Load suspicious users
-suspicious_df = pd.read_sql(""" 
-                 SELECT * FROM fraud_users_view
+# Load user scores 
+userscore_df = pd.read_sql(""" 
+                 SELECT * FROM scoring_users
                  """ , conn)
 
-print(suspicious_df.head())
+print(userscore_df.head())
 
 # Load reviews
 review_df = pd.read_sql(""" 
@@ -24,33 +24,40 @@ review_df = pd.read_sql("""
 
 print(review_df.head())
 
-fraud_counts = suspicious_df['fraud_score'].value_counts().sort_index()
+fraud_counts = userscore_df['fraud_score'].value_counts().sort_index()
 
-fraud_counts = fraud_counts.reindex([0,1,2,3], fill_value=0)
-
-# Plot suspicious user score Vs number of people
+# Plot user score Vs number of people
 fraud_counts.plot(kind="bar")
 plt.title("Fraud score distribution")
 plt.xlabel("Suspicious users (on scale of 0 to 3)")
 plt.ylabel("Number of people")
+plt.yscale('log')
 plt.savefig("fraud_score.png")
 plt.show()
 
 # Most affected products
 affected_products = pd.read_sql("""
-    SELECT r.product_id, COUNT(*) AS 
-    suspicious_reviews FROM reviews r
-    JOIN fraud_users_view f
-    ON r.user_id = f.user_id
-    WHERE f.fraud_score >=2
-    GROUP BY r.product_id
-    ORDER BY COUNT(*) DESC
-    LIMIT 10""", conn)
+    WITH suspicious_reviews AS(
+SELECT r.product_id, COUNT(*) AS suspicious_review_cnt
+FROM reviews r JOIN
+scoring_users s 
+ON s.user_id = r.user_id
+WHERE s.fraud_score >= 2
+GROUP BY r.product_id
+),
+rankings AS (
+SELECT * , DENSE_RANK() OVER 
+(ORDER BY suspicious_review_cnt DESC) AS ranks
+FROM suspicious_reviews)
+SELECT product_id, suspicious_review_cnt
+FROM rankings
+WHERE ranks <=5
+ORDER BY suspicious_review_cnt DESC """, conn)
 
-sns.barplot(data=affected_products, x="suspicious_reviews", y="product_id")
-plt.title("Affected products")
+sns.barplot(data=affected_products, x="suspicious_review_cnt", y="product_id")
+plt.title("Most affected products")
 plt.tight_layout()
-plt.savefig("affected_products.png")
+# plt.savefig("affected_products.png")
 plt.show()
 
 # % suspicious reviews on products
@@ -58,21 +65,29 @@ percentage_df = pd.read_sql(""" WITH total_reviews AS (
 SELECT product_id, COUNT(*) AS total
 FROM reviews
 GROUP BY product_id
-)
-SELECT r.product_id, COUNT(*) AS suspicious_reviews,
-t.total, ROUND(COUNT(*)*100/t.total,2)
+),
+suspicious_reviews AS (
+SELECT r.product_id, t.total, 
+COUNT(*) AS suspicious_review_cnt,
+ROUND(COUNT(*)*100.0/t.total,1)
 AS percent_suspicious_reviews
-FROM reviews r
-JOIN total_reviews t
-ON t.product_id = r.product_id
-JOIN fraud_users_view f
-ON f.user_id = r.user_id
-WHERE f.fraud_score >=2 AND t.total >= 20
-GROUP BY r.product_id 
-ORDER BY ROUND(COUNT(*)*100/t.total,2) DESC
-LIMIT 10""", conn)
-
-percentage_df['percent_suspicious_reviews'] = percentage_df['percent_suspicious_reviews'].astype(float)
+FROM reviews r JOIN total_reviews t
+ON r.product_id = t.product_id 
+JOIN scoring_users s 
+ON r.user_id = s.user_id
+WHERE s.fraud_score >=2 AND t.total > 20
+GROUP BY r.product_id, t.total
+), 
+ranking AS (
+SELECT * , DENSE_RANK() OVER 
+(ORDER BY percent_suspicious_reviews DESC) AS ranks
+FROM suspicious_reviews
+)
+SELECT product_id, total , suspicious_review_cnt,
+percent_suspicious_reviews
+FROM ranking
+WHERE ranks <= 5
+ORDER BY percent_suspicious_reviews DESC""", conn)
 
 percentage_df = percentage_df.sort_values(by="percent_suspicious_reviews", ascending=False)
 
@@ -83,17 +98,21 @@ plt.savefig("percentage_suspicious_reviews.png")
 plt.show()
 
 # Rating distribution
-rating_df = pd.read_sql(""" SELECT f.fraud_score,
-    r.rating, COUNT(*) AS review_count
-    FROM reviews r JOIN fraud_users_view f
-    ON r.user_id = f.user_id
-    GROUP BY f.fraud_score, r.rating
+rating_df = pd.read_sql(""" SELECT s.fraud_score , r.rating,
+COUNT(*) AS review_count
+FROM scoring_users s JOIN
+reviews r
+ON r.user_id = s.user_id
+WHERE r.rating IN (1,5)
+GROUP BY s.fraud_score, r.rating
+ORDER BY s.fraud_score DESC
                     """, conn)
 
 pivot_df = rating_df.pivot(index='fraud_score', columns='rating', values='review_count')
 pivot_df.plot(kind="bar", stacked=True)
-plt.title("Rating Distribution by Fraud Score")
-plt.ylabel("Count")
+plt.title("Extreme Rating Behavior Among Users by Fraud Score")
+plt.ylabel("Number of people")
+plt.yscale('log')
 plt.savefig("rating_distribution.png")
 plt.show()
 
@@ -129,9 +148,8 @@ plt.tight_layout()
 plt.savefig("spike_detection.png")
 plt.show()
 
-# Export data to excel
 with pd.ExcelWriter('review_fraud_detection.xlsx') as writer:
-    suspicious_df.to_excel(writer, sheet_name="suspicious_users", index=False)
+    userscore_df.to_excel(writer, sheet_name="user_scores", index=False)
     affected_products.to_excel(writer, sheet_name="affected_products", index=False)
     percentage_df.to_excel(writer, sheet_name="percentage_suspicious_reviews", index=False)
     rating_df.to_excel(writer, sheet_name="rating_distribution", index=False)
